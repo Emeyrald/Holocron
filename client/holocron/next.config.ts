ba1e33025422a00fc71890d324e7d04301c1f0a3
Alec Joseph <linkandmarth789@gmail.com>
2026-03-19 04:30:41 -0700

    Fix config for output

--- client/holocron/next.config.ts
+++ client/holocron/next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
+const repo = "/holocron";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
-  basePath: isProd ? "/holocron" : "",
-  assetPrefix: isProd ? "/holocron/" : "",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? repo : "",
+  assetPrefix: isProd ? `${repo}/` : "",
 };
 
 export default nextConfig;
