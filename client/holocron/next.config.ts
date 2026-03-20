883d4347050e0d1f78d93b12bb3442ac3c2b4a71
Alec Joseph <linkandmarth789@gmail.com>
2026-03-19 19:32:54 -0700

    Update next.config.ts for vercel deployment

--- client/holocron/next.config.ts
+++ client/holocron/next.config.ts
@@ -1,15 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-const repo = "/Holocron";
-
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? repo : "",
-  assetPrefix: isProd ? `${repo}/` : "",
 };
 
 export default nextConfig;
